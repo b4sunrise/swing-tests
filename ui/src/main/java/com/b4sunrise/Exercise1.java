966870dd3bb68bce7922f2d26297fdc6e1e655b4
Mehdi Afsari <mahdi.afsari@gmail.com>
2015-03-08 17:07:33 +0330

    initial commit

---
+++ ui/src/main/java/com/b4sunrise/Exercise1.java
@@ -0,0 +1,12 @@
+package com.b4sunrise;
+
+/**
+ * @author Mehdi Afsari Kashi
+ * @version 1.0.0
+ *          <p/>
+ *          Creation Date:  2015/03/08
+ * @since 1.0.0
+ */
+public class Exercise1 {
+
+}
